8070e093326f7276a7406292b0d32375309d4444
MohamedRiad98 <133154356+MohamedRiad98@users.noreply.github.com>
2023-10-21 00:26:07 +0300

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,47 +1,62 @@
 #include "main.h"
-#include <unistd.h>
-/**
- * _printf - Emulate the original.
- *
- * @format: Format by specifier.
- *
- * Return: count of chars.
- */
+#include <stdio.h>
+#include <stdarg.h>
+
 int _printf(const char *format, ...)
 {
-	int i = 0, count = 0, count_fun;
-	va_list args;
-
-	va_start(args, format);
-	if (!format || (format[0] == '%' && !format[1]))
-		return (-1);
-	if (format[0] == '%' && format[1] == ' ' && !format[2])
-		return (-1);
-	while (format[i])
-	{
-		count_fun = 0;
-		if (format[i] == '%')
-		{
-			if (!format[i + 1] || (format[i + 1] == ' ' && !format[i + 2]))
-			{
-				count = -1;
-				break;
-			}
-			count_fun += get_function(format[i + 1], args);
-			if (count_fun == 0)
-				count += _putchar(format[i + 1]);
-			if (count_fun == -1)
-				count = -1;
-			i++;
-		}
-		else
-		{
-			(count == -1) ? (_putchar(format[i])) : (count += _putchar(format[i]));
-		}
-		i++;
-		if (count != -1)
-			count += count_fun;
-	}
-	va_end(args);
-	return (count);
+    va_list args;
+    va_start(args, format);
+    
+    int count = 0;
+    char c;
+    
+    while (*format != '\0')
+    {
+        if (*format == '%')
+        {
+            format++;
+            
+            switch (*format)
+            {
+                case 'c':
+                    c = (char) va_arg(args, int);
+                    putchar(c);
+                    count++;
+                    break;
+                
+                case 's':
+                {
+                    char *str = va_arg(args, char *);
+                    while (*str != '\0')
+                    {
+                        putchar(*str);
+                        count++;
+                        str++;
+                    }
+                    break;
+                }
+                
+                case '%':
+                    putchar('%');
+                    count++;
+                    break;
+                
+                default:
+                    putchar('%');
+                    putchar(*format);
+                    count += 2;
+                    break;
+            }
+        }
+        else
+        {
+            putchar(*format);
+            count++;
+        }
+        
+        format++;
+    }
+    
+    va_end(args);
+    return count;
 }
